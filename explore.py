b8ad22a073dc9f2da34e6e608a07242d7553b37e
Arnav Jaiswal <arnavjaiswal149@gmail.com>
2023-11-14 02:12:02 +0800

    Added functionality and updated UI components

--- explore.py
+++ explore.py
@@ -1,8 +1,46 @@
+import ast
 import json
 import re
 import psycopg2
 import matplotlib.pyplot as plt
 import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
+
+
+class Node:
+    """
+    Represents a node in a query execution plan.
+    """
+
+    def __init__(self, qep_json):
+        """
+        Initializes the Node with data from a JSON object.
+
+        :param plan_data: A dictionary representing the plan data for this node.
+        """
+        self.node_type = qep_json.get("Node Type")
+        self.plan = {key: value for key, value in qep_json.items(
+        ) if key == "Relation Name" or key == "Hash Cond" or key == "Parent Relationship"}
+        self.children = [Node(child) for child in qep_json.get("Plans", [])]
+
+    def __str__(self):
+        """
+        Returns a string representation of the node.
+        """
+        child_str = ', '.join([str(child) for child in self.children])
+        return f"Node Type: {self.node_type}, Plan: {self.plan}, Children: [{child_str}]"
+
+    def print_tree(self, level=0):
+        """
+        Prints the node and its children in a tree-like structure.
+
+        :param level: The current depth level in the tree (used for indentation).
+        """
+        indent = "\t" * level  # Two spaces per level of depth
+        node_info = f"{indent}Node:\t{self.node_type}\n{indent}Plan:\t{self.plan}"
+        print(node_info)
+        for child in self.children:
+            child.print_tree(level + 1)
 
 
 def connect_to_db():
@@ -65,6 +103,7 @@ def execute_query(conn, query):
                             FORMAT JSON \
                         ) {query}")
         qep = cursor.fetchall()
+        # print(qep)
         qep = make_json_parsable(str(qep[0][0]))
         return qep
 
@@ -73,58 +112,134 @@ def execute_query(conn, query):
         return None
 
 
-def get_disk_blocks_accessed(conn, query):
-    """
-    Retrieves disk block access information for a given SQL query.
+def get_block_contents(conn, relation, block_id):
+    """Get contents of block with block_id of relation.
 
-    Parameters:
-    conn (psycopg2.connection): A connection to the PostgreSQL database.
-    query (str): The SQL query for which disk block access information is to be retrieved.
-
-    Returns:
-    dict: A dictionary containing disk block access information.
-    """
-    # Execute the provided SQL query
-    with conn.cursor() as cursor:
-        cursor.execute(query)
-        # Optionally, fetch and use query results if needed
-        # query_results = cursor.fetchall()
-
-    # Query the statistics view to get block access info
-    stats_query = """
-    SELECT 
-        relname, 
-        heap_blks_read,  -- Blocks read from disk
-        heap_blks_hit    -- Blocks found in cache
-    FROM 
-        pg_statio_user_tables;
+    Keyword arguments:
+    block_id -- block_id of block
+    relation -- relation that contains block
+    Return: list of tuples with block_id of relation
     """
 
-    with conn.cursor() as cursor:
-        cursor.execute(stats_query)
-        stats_results = cursor.fetchall()
-
-    # Format the results into a dictionary
-    block_access_info = {
-        "table": [],
-        "blocks_read": [],
-        "blocks_hit": []
-    }
-    for row in stats_results:
-        block_access_info["table"].append(row[0])
-        block_access_info["blocks_read"].append(row[1])
-        block_access_info["blocks_hit"].append(row[2])
-
-    for k,v in block_access_info.items():
-        print(k,':',v)
-    return block_access_info
+    cursor = conn.cursor()
+    query = f"""
+    SELECT ctid, *
+    FROM {relation}
+    WHERE (ctid::text::point)[0]::bigint = {block_id};
+    """
+    cursor.execute(query)
+    content = cursor.fetchall()
+    if content:
+        print(relation, block_id, 'content:', content)
+        return content
+    else:
+        raise ValueError("No data found for the given block ID.")
 
 
-def parse_and_visualize_qep(qep_json_str):
+def get_relation_block_ids(conn, relation_name):
+    """Get distinct block IDs for a given relation.
+
+    Keyword arguments:
+    relation_name -- name of the relation
+    Return: list of block IDs
+    """
+    cursor = conn.cursor()
+    query = f"""
+    SELECT DISTINCT (ctid::text::point)[0]::bigint AS block_id
+    FROM {relation_name};
+    """
+    cursor.execute(query)
+    out = cursor.fetchall()
+    if out is not None:
+        def group_consecutive_numbers_in_place(lst):
+            i = 0
+            while i < len(lst) - 1:
+                # Check if current and next elements are consecutive
+                if lst[i + 1] == lst[i] + 1:
+                    start = i
+                    # Find the end of the consecutive sequence
+                    while i < len(lst) - 1 and lst[i + 1] == lst[i] + 1:
+                        i += 1
+                    # Replace the start of the sequence with the tuple
+                    lst[start] = (lst[start], lst[i])
+                    # Delete the rest of the sequence
+                    del lst[start + 1:i + 1]
+                else:
+                    i += 1
+            return lst
+
+        ls = group_consecutive_numbers_in_place(
+            sorted([row[0] for row in out]))
+        print(relation_name, 'blocks:', ls)
+        return ls
+    else:
+        raise ValueError("No blocks found for the given relation.")
+
+
+def get_disk_blocks_accessed(conn, qep_json_str):
+    blocks_accessed = {}
+    qep = getAllRelationsInfo(qep_json_str=qep_json_str)
+
+    def process_node(node):
+        match node.node_type:
+            case "Seq Scan" | "Parallel Seq Scan":
+                relation_name = node.plan["Relation Name"]
+                blocks_accessed[relation_name] = {
+                    block_id
+                    for block_id in get_relation_block_ids(conn, relation_name)
+                }
+
+            case "Index Scan":
+                relation_name = node.plan["Relation Name"]
+                index_cond = node.plan.get(
+                    "Index Cond") or node.plan.get("Filter")
+                if index_cond:
+                    with conn.cursor() as cursor:
+                        cursor.execute(
+                            f"SELECT ctid, * FROM {relation_name} WHERE {index_cond};"
+                        )
+                        records = cursor.fetchall()
+                        block_ids = set()
+                        for record in records:
+                            block_id, _ = ast.literal_eval(record[0])
+                            block_ids.add(block_id)
+                        blocks_accessed[relation_name] = block_ids
+
+            # TODO: may need updates
+            case "Hash Join" | "Merge Join" | "Nested Loop":
+                # For join operations, process each child as they likely involve scans
+                for child in node.children:
+                    child_blocks_accessed = process_node(child)
+                    for relation, block_ids in child_blocks_accessed.items():
+                        if relation in blocks_accessed:
+                            blocks_accessed[relation].update(block_ids)
+                        else:
+                            blocks_accessed[relation] = block_ids
+
+            case _:
+                # Generic handler for other node types
+                for child in node.children:
+                    child_blocks_accessed = process_node(child)
+                    for relation, block_ids in child_blocks_accessed.items():
+                        if relation in blocks_accessed:
+                            blocks_accessed[relation].update(block_ids)
+                        else:
+                            blocks_accessed[relation] = block_ids
+
+        return blocks_accessed
+
+    # Starting the recursion with the root node
+    return process_node(qep[0])
+
+
+def visualize_qep(qep_json_str):
     # Parse the JSON string
     try:
         qep_json = json.loads(qep_json_str)
         print('JSON parsed!')
+        with open('plan.json', 'w') as outfile:
+            json.dump(qep_json, outfile)
+
     except json.JSONDecodeError as e:
         print('Error in JSON:', e)
         return
@@ -147,7 +262,7 @@ def parse_and_visualize_qep(qep_json_str):
 
             # Connect to parent node if exists
             if parent_label:
-                graph.add_edge(current_label,parent_label)
+                graph.add_edge(current_label, parent_label)
 
             # Process child nodes if any
             if 'Plans' in node and isinstance(node['Plans'], list):
@@ -158,9 +273,28 @@ def parse_and_visualize_qep(qep_json_str):
     for entry in qep_json:
         process_node(entry['Plan'])
 
+    # TODO: see if can be made better than what it is right now (replicate pgadmin graph though optional)
     # Visualization
-    pos = nx.spring_layout(graph)
+    pos = graphviz_layout(graph)
     nx.draw(graph, pos, with_labels=True, node_color='skyblue', edge_color='black',
             node_size=2000, arrowstyle='->', arrowsize=20)
     plt.title('Query Execution Plan')
     plt.show()
+
+
+def getAllRelationsInfo(qep_json_str):
+    qep_json = json.loads(qep_json_str)
+    # try:
+    #     cursor = conn.cursor()
+    #     cursor.execute(
+    #                 f"SELECT DISTINCT (ctid::text::point)[0]::bigint as block_id \
+    #                 FROM {relation_name};"
+    #             )
+    #     out = cursor.fetchall()
+    # except Exception as e:
+    #         print(f"Error retrieving relation block info: {e}")
+    #         return None
+    nodes = [Node(plan["Plan"]) for plan in qep_json]
+    for node in nodes:
+        node.print_tree()
+    return nodes
--- interface.py
+++ interface.py
@@ -1,53 +1,152 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QHBoxLayout, QMainWindow, QLabel, QTextEdit, QPushButton, QVBoxLayout, QWidget, QSizePolicy, QScrollArea, QApplication
 
-from explore import connect_to_db, execute_query, get_disk_blocks_accessed, parse_and_visualize_qep
+from explore import *
+
+# TODO: add functionality for another window to retrieve user db details (can follow ref github for this)
+
 
 class SQLQueryExecutor(QMainWindow):
     def __init__(self):
         super().__init__()
+        self._setup_main_window()
+        self._setup_central_widget()  # Call this to set up the central widget
+        self._setup_query_components()
+        self._setup_disk_block_components()
 
+    def _setup_main_window(self):
+        """Set up the main window properties."""
         self.setWindowTitle("SQL Query Executor")
-        self.setMinimumSize(400, 200)
+        self.setMinimumSize(1700, 1000)
 
+    def _setup_central_widget(self):
+        """Set up the central widget and main layout."""
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
+        self.main_layout = QHBoxLayout(central_widget)
 
-        layout = QVBoxLayout()
+    def _setup_query_components(self):
+        """Set up the components for SQL query input."""
+        self.queryInputLayout = QVBoxLayout()
 
         query_label = QLabel("Enter SQL Query:")
-        layout.addWidget(query_label)
+        self.queryInputLayout.addWidget(query_label)
 
         self.query_text = QTextEdit()
-        layout.addWidget(self.query_text)
+        self.queryInputLayout.addWidget(self.query_text)
 
-        submit_button = QPushButton("Submit")
-        submit_button.clicked.connect(self.on_submit_query)
-        layout.addWidget(submit_button)
+        self.submit_button = QPushButton("Execute Query")
+        self.submit_button.clicked.connect(self.on_submit_query)
+        self.queryInputLayout.addWidget(self.submit_button)
 
-        # Widgets for displaying disk block information
+        self.quit_button = QPushButton("Quit", self)
+        self.quit_button.clicked.connect(lambda: QApplication.quit())
+        self.quit_button.setSizePolicy(
+            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed,)
+        self.queryInputLayout.addWidget(self.quit_button)
+
+        self.main_layout.addLayout(self.queryInputLayout, stretch=2)
+
+    def _setup_disk_block_components(self):
+        """Set up components for displaying disk block information."""
+        self.disk_block_layout = QVBoxLayout()
         self.disk_block_label = QLabel("Disk Block Information:")
-        layout.addWidget(self.disk_block_label)
+        self.disk_block_layout.addWidget(self.disk_block_label)
 
-        self.disk_block_text = QTextEdit()
-        self.disk_block_text.setReadOnly(True)
-        layout.addWidget(self.disk_block_text)
+        self.relation_columns = QHBoxLayout()
+        self.relation_blocks_scroll_area = QScrollArea()
+        self.relation_blocks_scroll_area.setWidgetResizable(True)
+        self.relation_blocks_container = QWidget()
+        self.relation_blocks_container.setLayout(self.relation_columns)
+        self.relation_blocks_scroll_area.setWidget(
+            self.relation_blocks_container)
 
-        # Button to display disk block information
-        show_blocks_button = QPushButton("Show Disk Blocks Info")
-        show_blocks_button.clicked.connect(self.show_disk_block_info)
-        layout.addWidget(show_blocks_button)
+        self.disk_block_layout.addWidget(self.relation_blocks_scroll_area)
 
-        central_widget.setLayout(layout)
+        # Setup for block contents area
+        self.block_contents = QScrollArea()
+        self.block_contents.setWidgetResizable(True)
+        self.block_contents_container = QWidget()
+        self.block_contents_layout = QVBoxLayout()
+        self.block_contents_container.setLayout(self.block_contents_layout)
+        self.block_contents.setWidget(self.block_contents_container)
+
+        # Add the scroll area for block contents to the disk block layout
+        self.disk_block_layout.addWidget(self.block_contents)
+
+        self.main_layout.addLayout(self.disk_block_layout, stretch=3)
 
     def on_submit_query(self):
         query = self.query_text.toPlainText()
         conn = connect_to_db()
         qep = execute_query(conn, query)
-        parse_and_visualize_qep(qep)
+        getAllRelationsInfo(qep)
+        self.show_disk_block_info()
+        visualize_qep(qep)
 
     def show_disk_block_info(self):
-        # Get the current query from the QTextEdit widget
         query = self.query_text.toPlainText()
         conn = connect_to_db()
-        disk_blocks_info = get_disk_blocks_accessed(conn, query)
-        
+        qep = execute_query(conn, query)
+        disk_blocks_info = get_disk_blocks_accessed(conn, qep)
+
+        # Clear existing columns
+        for i in reversed(range(self.relation_columns.count())):
+            widget_to_remove = self.relation_columns.itemAt(i).widget()
+            if widget_to_remove is not None:
+                widget_to_remove.deleteLater()
+
+        # Create columns for each relation
+        for relation, block_ids in disk_blocks_info.items():
+            relations_layout = QVBoxLayout()
+
+            relation_label = QLabel(f"Relation: {relation}")
+            relations_layout.addWidget(relation_label)
+
+            relation_blocks_scroll_area = QScrollArea()
+            relation_blocks_scroll_area.setWidgetResizable(True)
+            relation_blocks_container = QWidget()
+            relation_blocks_layout = QVBoxLayout(relation_blocks_container)
+            relation_blocks_scroll_area.setWidget(relation_blocks_container)
+
+            # Create buttons for each block ID
+            for block_id in block_ids:
+                button = QPushButton(f"Block ID: {block_id}")
+                button.setSizePolicy(
+                    QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding)
+                # Connect button to function
+                button.clicked.connect(
+                    lambda: self.show_block_content(conn, relation, block_id))
+                relation_blocks_layout.addWidget(button)
+
+            relations_layout.addWidget(relation_blocks_scroll_area)
+            relations_column_widget = QWidget()
+            relations_column_widget.setLayout(relations_layout)
+            self.relation_columns.addWidget(relations_column_widget)
+
+    ''' TODO:
+    - fix 1 below function to update properly when clicking some other block
+    
+    - fix 2
+        1. update `get_block_contents` to handle ranged ids (those shown in brackets eg (322,3324) is blocks from id 322 to 3324)
+        2. currently for single block ids shown as vertically ([relation block] id heading label followed by the content text objects)
+        3. for ranged queries show sequentially ([relation name] block id [start ...  end] heading label followed by the content text objects for each id)
+    '''
+
+    def show_block_content(self, conn, relation, block_id):
+        content = get_block_contents(conn, relation, block_id)
+
+        # # Clear existing widgets from the layout
+        for i in reversed(range(self.block_contents_layout.count())):
+            widget_to_remove = self.block_contents_layout.itemAt(i).widget()
+            if widget_to_remove is not None:
+                self.block_contents_layout.removeWidget(widget_to_remove)
+                widget_to_remove.deleteLater()
+
+        self.block_contents_layout.addWidget(
+            QLabel(f"Relation: {relation} | Block ID: {block_id}"))
+
+        for item in content:
+            block_content_text = QTextEdit()
+            block_content_text.setReadOnly(True)
+            block_content_text.setPlainText(str(item).replace("  ", " "))
+            self.block_contents_layout.addWidget(block_content_text)
