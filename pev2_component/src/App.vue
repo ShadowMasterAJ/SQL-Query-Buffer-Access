d362461dce2ad41a368568df8bbec64bf5e7ea80
Darren <81216775+darrenc-35@users.noreply.github.com>
2023-11-15 18:53:44 +0800

    setup VueJS component PEV2

---
+++ pev2_component/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { Plan } from 'pev2';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'pev2/dist/style.css';
+
+export default {
+  data() {
+    return {
+    #TODO: INSERT PLAN HERE
+      plan: "Seq Scan on foo  (cost=0.00..155.00 rows=10000 width=4)",
+    };
+  },
+  components: {
+    Plan,
+  },
+};
+</script>
